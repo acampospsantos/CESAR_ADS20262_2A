8e4ac5d96b43ea76264d7abe94036984dee33261
antsu comuna <acampospsantos@hotmail.com>
2026-08-12 15:33:02 -0300

    Atualizacao: Exercicio - calculadora

--- Exercícios/calculadora.c
+++ Exercícios/calculadora.c
@@ -1,18 +1,86 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int menu(){
-    printf("\n--- CALCULADORA ---\n");
-    printf("1 - SOMA\n");
-    printf("2 - SUBTRACAO\n");
-    printf("3 - MULTIPLICAR\n");
-    printf("4 - DIVISAO\n");
+// --- OPERAÇÕES ---
+void soma(){
+    int numero1;
+    int numero2;
     
+    printf("\n--- SOMA ---\n");
+    printf("Digite o primeiro numero: ");
+    scanf("%i", &numero1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%i", &numero2);
+
+    int resultado = numero1 + numero2;
+    printf("\nO resultado foi %i", resultado);
+}
+
+void subtracao(){
+    int numero1;
+    int numero2;
+
+    printf("\n--- SUBTRACAO ---\n");
+
+    printf("Digite o primeiro numero: ");
+    scanf("%i", &numero1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%i", &numero2);
+
+    int resultado = numero1 - numero2;
+    printf("\nO resultado foi %i", resultado);
+}
+
+void multiplicacao(){
+    int numero1;
+    int numero2;
+
+    printf("\n--- MULTIPLICACAO ---\n");
+
+    printf("Digite o primeiro numero: ");
+    scanf("%i", &numero1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%i", &numero2);
+
+    int resultado = numero1 * numero2;
+    printf("\nO resultado foi %i", resultado);
+}
+
+void divisao(){
+    float numero1;
+    float numero2;
+
+    printf("\n--- DIVISAO ---\n");
+
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &numero1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%f", &numero2);
+
+    while(numero2 == 0){
+        printf("INDETERMINAÇÃO\n");
+        printf("Digite novamente: ");
+        scanf("%f", &numero2);
+    }
+
+    float resultado = numero1 / numero2;
+    printf("\nO resultado foi %.2f", resultado);
+}
+
+
+//ESCOLHE A OPERAÇÃO ARITMÉTICA
+void escolha(){
+
     int escolha;
     printf("Qual opcao deseja?: ");
     scanf("%i", &escolha);
     
-    while (escolha < 1 || escolha > 5){
-        printf("## Opcao inválida ##");
+    while (escolha < 1 || escolha > 4){
+        printf("## Opcao inválida ##\n");
         printf("Digite novamente: ");
         scanf("%i", &escolha);
     }
@@ -27,84 +95,32 @@ int menu(){
     } else if (escolha == 4){
         divisao();
     }
-    return 0;
 }
 
-int soma(){
-    int numero1;
-    int numero2;
-    
-    printf("\n--- SOMA ---\n");
-    printf("Digite o primeiro numero: ");
-    scanf("%i", &numero1);
 
-    printf("Digite o segundo numero: ");
-    scanf("%i", &numero2);
+// MENU
+void menu(){
+    printf("\n--- CALCULADORA ---\n");
+    printf("1 - SOMA\n");
+    printf("2 - SUBTRACAO\n");
+    printf("3 - MULTIPLICAR\n");
+    printf("4 - DIVISAO\n");
 
-    int resultado = numero1 + numero2;
-    printf("\nO resultado foi %i", resultado);
-    return 0;
-}
-
-int subtracao(){
-    int numero1;
-    int numero2;
-
-    printf("\n--- SUBTRACAO ---\n");
-
-    printf("Digite o primeiro numero: ");
-    scanf("%i", &numero1);
-
-    printf("Digite o segundo numero: ");
-    scanf("%i", &numero2);
-
-    int resultado = numero1 - numero2;
-    printf("\nO resultado foi %i", resultado);
-    return 0;
-}
-
-int multiplicacao(){
-    int numero1;
-    int numero2;
-
-    printf("\n--- MULTIPLICACAO ---\n");
-
-    printf("Digite o primeiro numero: ");
-    scanf("%i", &numero1);
-
-    printf("Digite o segundo numero: ");
-    scanf("%i", &numero2);
-
-    int resultado = numero1 * numero2;
-    printf("\nO resultado foi %i", resultado);
-    return 0;
-}
-
-int divisao(){
-    int numero1;
-    int numero2;
-
-    printf("\n--- DIVISAO ---\n");
-
-    printf("Digite o primeiro numero: ");
-    scanf("%i", &numero1);
-
-    printf("Digite o segundo numero: ");
-    scanf("%i", &numero2);
-
-    while(numero2 == 0){
-        printf("INDETERMINAÇÃO\n");
-        printf("Digite novamente: ");
-        scanf("%i", numero2);
-    }
-
-    int resultado = numero1 / numero2;
-    printf("\nO resultado foi %i", resultado);
-    return 0;
+    escolha();
 }
 
 
 int main() {
-    menu();
+    bool condicaoParada = true;
+    int parada;
+    while(condicaoParada == true){
+        menu();
+        printf("\nDeseja encerrar o programa? (0 - pra sair): ");
+        scanf("%i", &parada);
+        if (parada == 0){
+            condicaoParada = false;
+        } 
+    }
+    printf("\nSaindo do programa...");
     return 0;
 }
